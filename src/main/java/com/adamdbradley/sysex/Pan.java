d8492d272874b4b3afd421d6d0c1301bfe251afc
Dr. Adam D. Bradley <adam.d.bradley@gmail.com>
2016-09-23 20:02:00 -0700

    Add nominals to Pan

--- src/main/java/com/adamdbradley/sysex/Pan.java
+++ src/main/java/com/adamdbradley/sysex/Pan.java
@@ -2,7 +2,7 @@ package com.adamdbradley.sysex;
 
 /**
  * Identifies a MIDI Continuous Control pan value.
- * Nominals TBD -- is 64 C (0)? XXX
+ * Converts between nominal (-63..63) and wire (1..64..127) formats.
  */
 public class Pan extends ContinuousControlValue {
 
@@ -19,12 +19,11 @@ public class Pan extends ContinuousControlValue {
     };
 
     /**
-     * 
      * @param value 0..127
      * @return
      */
     public static Pan of(final int value) {
-        return pan[value];
+        return pan[value + 64];
     }
 
 }
