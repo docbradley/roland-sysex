829a12032c2366a297566140572ca92c63abe050
Dr. Adam D. Bradley <adam.d.bradley@gmail.com>
2016-11-26 12:44:35 -0800

    Tweak JavaDOc wording

--- src/main/java/com/adamdbradley/midi/Update.java
+++ src/main/java/com/adamdbradley/midi/Update.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.adamdbradley.midi.message.Message;
 
 /**
- * Encapsulates any object capable of specifying a transmittable update as
+ * Encapsulates any object capable of specifying a transmissible update as
  * a series of one or more {@link Message}s.
  */
 public interface Update {
